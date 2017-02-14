5958d5f46f05a6f076597055c99c73cce612bf93
webianks <webianks@gmail.com>
2017-02-14 12:21:45 +0530

    Updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('1520764620')
 })
 
 // for Facebook verification
