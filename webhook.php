73f43d6714b6c6167f334adcec67f7c0ddafc1e5
webianks <webianks@gmail.com>
2017-02-14 11:59:28 +0530

    restored

--- webhook.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
-
-if ($verify_token === '2181ae91-2249-4d41-938f-6a538b8107b8') {
-echo $challenge;
-}
