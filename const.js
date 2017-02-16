47411713323a24b75796b476234f9ccbace3a724
webianks <webianks@gmail.com>
2017-02-16 23:32:53 +0530

    added const.js

---
+++ const.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Wit.ai parameters
+const WIT_TOKEN = process.env.WIT_TOKEN;
+if (!WIT_TOKEN) {
+  throw new Error('missing WIT_TOKEN');
+}
+
+// Messenger API parameters
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+
+module.exports = {
+  WIT_TOKEN: WIT_TOKEN,
+  FB_PAGE_TOKEN: FB_PAGE_TOKEN,
+  FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
+};
