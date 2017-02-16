04fb02e2b22d1fd31eb084a875b8bc98ff46712f
webianks <webianks@gmail.com>
2017-02-16 23:15:07 +0530

    modified

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/webhook/', function (req, res) {
             sendGenericMessage(sender)
             continue
         }
-        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        sendTextMessage(sender, "You just said: " + text.substring(0, 200))
       }
       if (event.postback) {
         let text = JSON.stringify(event.postback)
